04d07193cd55b84e56d92124813265016ee1ddc5
Ella Thompson <fang98t@gmail.com>
2020-09-22 21:10:50 -0400

    variables and arrays added

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,101 @@
+// variables
+var charCount;
+var confirmNumber;
+var confirmSpecial;
+var confirmUpper;
+var confirmLower;
+
+// characters in variables arrays
+number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+special = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "{",
+  "}",
+  "[",
+  "]",
+  ";",
+  ":",
+  "'",
+  ",",
+  ".",
+  "?",
+  "/",
+  "`",
+  "~",
+  "|",
+  "<",
+  "=",
+  ">",
+];
+
+upper = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+lower = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
